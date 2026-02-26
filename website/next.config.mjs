618e794e22267cae4f5c995cdd43202ec4a2cce7
Jung <alex24922665@gmail.com>
2026-02-26 18:03:43 +0800

    fix paths issue

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Jung217', // Fixes GitHub pages subpath routing for assets
   images: {
     unoptimized: true,
   },
